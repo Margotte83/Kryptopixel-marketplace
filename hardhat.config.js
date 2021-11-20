1a3f39d676a00b65e1defe9880db7299ebbee258
Bertrand Juglas <b@juglas.name>
2021-11-20 17:57:38 +0100

    feat: add spark support

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy-ethers");
 const fs = require('fs');
 const privateKey = fs.readFileSync(".env").toString().trim() || "";
 //const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -9,19 +20,20 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    
-    mumbai: {
-      // Infura
-      //url: `https://polygon-mumbai.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mumbai.maticvigil.com",
+    spark: {
+      chainId: 123,
+      url: "https://rpc.fusespark.io",
       accounts: [privateKey]
     },
-    matic: {
-      // Infura
-      //url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mainnet.maticvigil.com",
-      accounts: [privateKey]
-    }
+
+    // mumbai: {
+    //   url: `https://polygon-mumbai.infura.io/v3/${infuraId}`,
+    //   accounts: [privateKey]
+    // },
+    // matic: {
+    //   url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+    //   accounts: [privateKey]
+    // }
     
   },
   solidity: {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 const hre = require("hardhat");
-//const fs = require('fs');
+const fs = require('fs');
 
 async function main() {
   const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
